2c0901781ec0881be3bc8ab11dd6536c50f99bb4
Tommy Huynh <TommysMAC@Tommys-MacBook-Pro.local>
2016-12-08 17:19:50 -0800

    finish

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,16 @@
 class HomeController < ApplicationController
   def index
-    @droplets = Droplet.all
-    # puts "NIGGER"
-    # @droplets.each do |droplet|
-    #   puts droplet.adventure
-    # end
+    @droplets = Droplet.all.where(:complete => false)
   end
 
-
+  def checkoff
+    droplet = Droplet.find(params[:droplet_id])
+    if droplet.complete
+      droplet.complete = false
+    else
+      droplet.complete = true
+    end
+    droplet.save!
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :droplets
   root 'home#index'
+
+  post 'checkoff', to: 'home#checkoff', :as => :checkoff
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'signup', to: 'home#create', :as => :user
 end
